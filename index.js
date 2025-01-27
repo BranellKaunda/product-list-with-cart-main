40358ff95bb02ab8b3d23c26b3933a6e35eeb783
BranellKaunda <branellk@gmail.com>
2025-01-27 16:52:30 +0100

    create an oder confirmation section

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function addToOrder(productName) {
     
       if (product.name === productName) {
         order.push(product);
-        console.log(order);
       }
 
     })
@@ -35,7 +34,7 @@ function displayOrder() {
 
             <div class="details"> 
               <p class="item-name">${item.name}</p>
-              <p class="price"><span style="color: hsl(14, 86%, 42%);">${item.quantity}x</span> @ $${Number((item.price * 100) / 100).toFixed(2)}</p>
+              <p class="price"><span style="color: hsl(14, 86%, 42%);">${item.quantity}x</span> @$${formatPrice(item.price)} $${formatPrice((item.quantity * item.price))}</p>
             </div>
             
             <div class="button-div">
@@ -60,9 +59,9 @@ function removeOrder() {
 
         order.forEach((item, index) => {
           if(item.name === btn.dataset.orderId) {
-            console.log('its working');
             order.splice(index, 1);
-            console.log(order);
+
+            //document.querySelector('.js-your-order').innerHTML = `Your Order (${item.quantity})`;
           }
 
           if(order.length === 0) {
@@ -73,6 +72,10 @@ function removeOrder() {
                 
                 <p>Your added items will appear here</p>
               </div>`;
+
+              document.querySelector('.js-total-container')
+              .innerHTML = '';
+
           }
         })
 
@@ -81,51 +84,46 @@ function removeOrder() {
     })
 }
 
-function comfirmOrder() {
-  let total = 0;
+function formatPrice(number) {
 
-  order.forEach((item) => {
-    total += item.price;
-  })
-
-  console.log(Number((total * 100) / 100).toFixed(2));
+  return Number((number * 100) / 100).toFixed(2);
 }
 
-comfirmOrder();
 
 function addQuantity(productName) {
-
   let itemQuantity = 0;
-  itemQuantity++;
-
+  
   order.forEach((item) => {
-    
     if (productName === item.name) {
+      itemQuantity++;
       item.quantity += itemQuantity;
-      document.querySelector('.js-order').innerHTML = '';
-      displayOrder();
+      document.querySelector('.js-order').innerHTML = ''; 
     }
 
   })
   
+  displayOrder();
   console.log(order);
 }
 
 function reduceQuantity(productName) {
-
   let itemQuantity = 0;
-  itemQuantity++;
-
-  order.forEach((item) => {
-    
+  
+  order.forEach((item) => { 
     if (productName === item.name) {
-      item.quantity -= itemQuantity;
+      itemQuantity--;
+      item.quantity += itemQuantity;
       document.querySelector('.js-order').innerHTML = '';
-      displayOrder();
+    }
+
+    //improve feature
+    if(item.quantity === 0) {
+      item.quantity = 1;
     }
 
   })
   
+  displayOrder();
   console.log(order);
 }
 
@@ -134,7 +132,6 @@ addButton.forEach((button) => {
   let quantity= 1;
   
   button.addEventListener('click', () => {
-    
     let productName = button.dataset.productName;
 
     if (!button.classList.contains('select-button')) {
@@ -148,7 +145,7 @@ addButton.forEach((button) => {
     }
 
     button.innerHTML = `
-      <button class="decrease js-decrease"><img src="assets/images/icon-decrement-quantity.svg" alt=""></button> 
+      <button class="decrease js-decrease" ><img src="assets/images/icon-decrement-quantity.svg" alt=""></button> 
         ${quantity}
       <button class="increase js-increase" data-product-name="${productName}"><img src="assets/images/icon-increment-quantity.svg" alt=""></button>`
     ;
@@ -172,7 +169,7 @@ addButton.forEach((button) => {
           orderQuantity--;
 
           if (quantity === 0) {
-            quantity = 0;
+            quantity = 1;
           } else if (quantity === -1) {
             quantity = 0;
           }
@@ -183,7 +180,47 @@ addButton.forEach((button) => {
         })
     });   
 
+    renderTotalHTML();
     document.querySelector('.js-your-order').innerHTML = `Your Order (${orderQuantity})`;
    
   })
 });
+
+async function renderTotalHTML() {
+  let totalPrice = 0;
+
+  order.forEach((item) => {
+    totalPrice += item.price;
+
+    document.querySelector('.js-total-container')
+    .innerHTML =`
+      <div class="total-container">
+        <div class="total">
+          Order Total
+        </div>
+
+        <div class="total-price">
+          $${formatPrice(item.quantity * totalPrice)}
+        </div>
+      </div>
+
+      <div class="order-description">
+        <div class="carbon-image"><img src="/assets/images/icon-carbon-neutral.svg" alt=""></div>
+        <div>
+          <p>This is a <span style="color:hsl(14, 65%, 9%); font-weight: 500">carbon neutral</span> delivery</p>
+        </div>
+      </div>
+
+      <div class="comfirm-oder">
+        <button class="js-confirm-order-button">Confirm Order</button>
+      </div>
+    `;
+
+    document.querySelector('.js-confirm-order-button').addEventListener('click', () => {
+     console.log('hello');
+    })
+
+  })
+
+
+}
